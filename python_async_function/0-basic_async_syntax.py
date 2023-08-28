b2a60dbddc5e5c9e6d95bb2fdd1b26efbf98ea76
Mishel450 <tomasmishel18@gmai.com>
2023-08-28 09:48:46 -0700

    task-0 1.1

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """task-0"""
     rand_float = uniform(0, max_delay)
     await asyncio.sleep(rand_float)
