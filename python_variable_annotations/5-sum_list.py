f28774af5f26ed868e42ddda747b463666203f50
Mishel450 <tomasmishel18@gmai.com>
2023-08-28 06:44:20 -0700

    task-5 1.1

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """task-3"""
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """takes a list of floats and sum them"""
     a = 0.00
     for i in input_list:
