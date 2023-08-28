24994b7c7b2964902133ad635b044f61a13009a2
Mishel450 <tomasmishel18@gmai.com>
2023-08-28 07:28:10 -0700

    task-9 1.1

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""task-8"""
+"""task-9"""
 from typing import Sequence, Iterable, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """task-9"""
     return [(i, len(i)) for i in lst]
