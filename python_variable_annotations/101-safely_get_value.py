18c1db8cf5733184835cbfa5896fca7c6899e9d0
Mishel450 <tomasmishel18@gmai.com>
2023-08-28 08:46:54 -0700

    task-101 1.1

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""task-100"""
+"""task-101"""
 from typing import Mapping, Any, Union, TypeVar
 
 T = TypeVar('T')
@@ -7,6 +7,7 @@ T = TypeVar('T')
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
+    """task-101"""
     if key in dct:
         return dct[key]
     else:
