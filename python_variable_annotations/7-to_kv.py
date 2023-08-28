5923bc8b4300f6f97ee9ea33fd9709dfb6c171e7
Mishel450 <tomasmishel18@gmai.com>
2023-08-28 07:06:05 -0700

    task-7 done

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""task-6"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """takes a string and a int/float and returns a tuple of them"""
+    return k, v
