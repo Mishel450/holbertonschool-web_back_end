49d82000233dc49db82141e2be3deda158c53a18
Mishel450 <tomasmishel18@gmai.com>
2023-08-28 07:09:26 -0700

    task-7 1.1

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """takes a string and a int/float and returns a tuple of them"""
-    return k, v
+    return (k, v)
