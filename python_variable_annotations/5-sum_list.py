5a214bf0ba23a2f4ea6cdf3ec6b67b5aad6e6d46
Mishel450 <tomasmishel18@gmai.com>
2023-08-28 06:41:50 -0700

    task-5 done

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""task-3"""
+
+
+def sum_list(input_list: list) -> float:
+    """takes a list of floats and sum them"""
+    a = 0.00
+    for i in input_list:
+        a += i
+    return a
