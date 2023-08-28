9095fe6cc1807cf783076a059e6f638b36aa4ab7
Mishel450 <tomasmishel18@gmai.com>
2023-08-28 11:26:30 -0700

    task-2 done

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""task-1"""
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    a_list = await wait_n(n, max_delay)
+    total_time = 0.00
+    for i in a_list:
+        total_time += i
+    the_time = total_time / n
+    return the_time
