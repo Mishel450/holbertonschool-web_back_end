b0f579b2509f4a94e40f85c52de90a072fef81a9
Mishel450 <tomasmishel18@gmai.com>
2023-08-29 10:28:53 -0700

    task-0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""task-0"""
+import asyncio
+from random import uniform
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """async_generator generates a 10 random floats"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        random_float = uniform(0, 10)
+        yield random_float
