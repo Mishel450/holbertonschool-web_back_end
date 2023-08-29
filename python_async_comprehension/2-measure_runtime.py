cd846f2b110c40cc7810ef58f021a3b0b1f4479b
Mishel450 <tomasmishel18@gmai.com>
2023-08-29 10:57:36 -0700

    task-2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""task-2"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """task-2"""
+    time1 = time.time()
+    await asyncio.gather(async_comprehension(),
+                   async_comprehension(),
+                   async_comprehension(),
+                   async_comprehension())
+    time2 = time.time()
+    total_time = time2 - time1
+    return (total_time)
