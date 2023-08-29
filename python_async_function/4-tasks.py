03aaddce97cee74f172b83e16845a5f5c8162d43
Mishel450 <tomasmishel18@gmai.com>
2023-08-29 10:14:11 -0700

    task-4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,9 +6,9 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """task-0"""
     list_random = []
     for _ in range(n):
-        list_random.append(task_wait_random(max_delay))
+        list_random.append(await task_wait_random(max_delay))
     return sorted(list_random)
