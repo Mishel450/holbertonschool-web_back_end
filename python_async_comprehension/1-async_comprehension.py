0cf806853d602dee24d785d39733c96e4ea5b1e8
Mishel450 <tomasmishel18@gmai.com>
2023-08-29 10:47:44 -0700

    task-1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""""""
+"""task-1"""
 import asyncio
 from typing import List
 
@@ -7,5 +7,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """task-1"""
     result: List[float] = [i async for i in async_generator()]
-    print(result)
+    return (result)
