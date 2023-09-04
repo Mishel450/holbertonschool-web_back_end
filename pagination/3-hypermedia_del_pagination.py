e401901f437983553e53b8e87f0e8eb1f11cbd76
Mishel450 <tomasmishel18@gmai.com>
2023-09-04 10:26:46 -0700

    task-3 done

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """returns a dict with the index and the next_index"""
+        self.indexed_dataset()
+        the_list = []
+        for i in self.__indexed_dataset:
+            if  i >= index and i <= index + page_size:
+                the_list.append(self.__indexed_dataset[i])
+        the_dict = {'index': index, 'data': the_list, 'page_size': page_size, 'next_index': index + page_size}
+        return the_dict
