2fee4533f52144414b76db0eb4d693376b628963
Mishel450 <tomasmishel18@gmai.com>
2023-09-04 09:41:23 -0700

    task-2 1.1

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -44,6 +44,7 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """gets the values and returns a dict"""
         self.dataset()
+        
         if self.__dataset[page - 1]:
             prev_page = None
         else:
