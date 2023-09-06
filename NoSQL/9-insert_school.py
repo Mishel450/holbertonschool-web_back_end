e02d406c0192f0b8dd1ec45ce54ffdf1f4be785c
Mishel450 <tomasmishel18@gmai.com>
2023-09-06 05:52:42 -0700

    task-9 1.1

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -8,7 +8,7 @@ def insert_school(mongo_collection, **kwargs):
     """creates a list and inserts a new document"""
     the_dict = {}
     new_id = uuid.uuid4()
-    for key, value in kwargs:
+    for key, value in kwargs.items():
         the_dict[key] = value
     the_dict['_id'] = new_id
     mongo_collection.insert_one(the_dict)
