b6cd1410996ce3776113d452da0a5bbec0727d0f
Mishel450 <tomasmishel18@gmai.com>
2023-09-06 05:46:46 -0700

    task-9 done

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""task-9"""
+import pymongo
+import uuid
+
+
+def insert_school(mongo_collection, **kwargs):
+    """creates a list and inserts a new document"""
+    the_dict = {}
+    new_id = uuid.uuid4()
+    for key, value in kwargs:
+        the_dict[key] = value
+    the_dict['_id'] = new_id
+    mongo_collection.insert_one(the_dict)
+    return new_id
