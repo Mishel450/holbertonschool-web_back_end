308b3d070d7354d272d46a26750ac91ea431421c
Mishel450 <tomasmishel18@gmai.com>
2023-09-06 06:12:03 -0700

    task-10 done

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""task-10"""
+import pymongo
+import uuid
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes the topics of a document"""
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
