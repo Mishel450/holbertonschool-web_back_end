57cf0822d4765de334804fd5ecf41acdabc47adc
Mishel450 <tomasmishel18@gmai.com>
2023-10-04 06:56:52 -0700

    task6 done

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
+  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)])
+    .then((results) => {
+      results.forEach((result) => {
+      Promise.resolve({status: result.status, value: result.value})
+    })
+  })
 }
 
 export default handleProfileSignup;
