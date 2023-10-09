9d3d470edcc6c9bba242e53e6764d03d9695c9f4
Mishel450 <tomasmishel18@gmai.com>
2023-10-09 10:16:08 -0700

    task-9 done

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const themap = new Map();
+  themap.set('Apples', 10);
+  themap.set('Tomatoes', 10);
+  themap.set('Pasta', 1);
+  themap.set('Rice', 1);
+  themap.set('Banana', 5);
+  return themap;
+}
