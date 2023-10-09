4466b80c9e94b78b2d977366070a39d39398d593
Mishel450 <tomasmishel18@gmai.com>
2023-10-09 09:34:19 -0700

    task-8 done

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,20 @@
 export default function cleanSet(set, startString = '') {
   const { length } = startString;
   let stringToReturn = '';
-  if (startString !== '') {
-    for (const i of set) {
-      if (i !== undefined) {
-        const check = i.startsWith(startString);
-        if (check === true) {
-          stringToReturn += `${i.substring(length)}-`;
+  if (startString instanceof String) {
+    if (startString !== '') {
+      for (const i of set) {
+        if (i !== undefined) {
+          const check = i.startsWith(startString);
+          if (check === true) {
+            stringToReturn += `${i.substring(length)}-`;
+          }
         }
       }
     }
-  }
-  if (stringToReturn.endsWith('-')) {
-    stringToReturn = stringToReturn.slice(0, -1);
+    if (stringToReturn.endsWith('-')) {
+      stringToReturn = stringToReturn.slice(0, -1);
+    }
   }
   return stringToReturn;
 }
